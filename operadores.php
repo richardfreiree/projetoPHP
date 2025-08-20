3127c25c1a1aa1c56dc72f3ecc04d4aaaa7a8cf5
richardfreiree <richardzzfreirezz@gmail.com>
2025-08-19 21:16:26 -0300

    trabalhando com operadores e variáveis em php

---
+++ operadores.php
@@ -0,0 +1,13 @@
+<?php 
+
+	echo("Trabalhando com operadores em PHP <br />");
+
+
+	echo ("Operadores de soma <br />");
+	echo ("Operadores de subtração <br />");
+	echo ("Operadores de multiplicação <br />");
+	echo ("Operadores de divisao <br />");
+
+	echo ("insira a operação desejada:  <br />");
+	
+ ?>
