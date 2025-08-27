7980737d43e967be19b169dc9eaf8acc317f1aaa
richardfreiree <richardzzfreirezz@gmail.com>
2025-08-26 21:22:13 -0300

    subindo calculadora em php

--- calculadora.php
+++ calculadora.php
@@ -3,26 +3,26 @@
 // calculadora
 	echo("============================CALCULADORA===================================== <br/> 1-Soma <br/> 2-Subtração <br/> 3-Divisão </br> 4-Multiplicação");
 
-	$escolha = 1;
-	$num1 = 20;
-	$num2 = 30;
+	$escolha = 3;
+	$num1 = 40;
+	$num2 = 10;
 	$resp = 0;
 	function somaValor($a,$b){
 		return($a + $b);
 
 	};
-		function subtracaoValor(){
+		function subtracaoValor($a, $b){
 		return($a - $b);
 
 	};
 
-	function divisaoValor(){
+	function divisaoValor($a, $b){
 	return($a / $b);
 
 
 	};
 
-	function multiplicacaoValor(){
+	function multiplicacaoValor($a, $b){
 		return($a * $b);
 
 
@@ -35,13 +35,22 @@
 	echo "<br/>A resposta da soma é: $resp";
 	}
 	elseif ($escolha == 2) {
-	subtracaoValor($num1, $num2);
+	$resp = subtracaoValor($num1, $num2);
+	echo "<br/><br/>Os numeros digitados foram $num1 e $num2";
+	echo "<br/>O calculo escolhido foi a Subtração";
+	echo "<br/>A resposta da subtração é: $resp";
 	}
 	elseif ($escolha == 3) {
-		divisaoValor($num1, $num2);
+	$resp =	divisaoValor($num1, $num2);
+	echo "<br/><br/>Os numeros digitados foram $num1 e $num2";
+	echo "<br/>O calculo escolhido foi a Divisão";
+	echo "<br/>A resposta da divisão é: $resp";
 	}
 	elseif ($escolha == 4) {
-		multiplicacaoValor($num1, $num2);
+	$resp =	multiplicacaoValor($num1, $num2);
+	echo "<br/><br/>Os numeros digitados foram $num1 e $num2";
+	echo "<br/>O calculo escolhido foi a Multiplição";
+	echo "<br/>A resposta da multiplicação é: $resp";
 	};
 
  ?>	
