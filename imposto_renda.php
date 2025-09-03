f8907084cb3d8d2c33b283a5142990c004e98efc
richardfreiree <richardzzfreirezz@gmail.com>
2025-09-02 21:14:31 -0300

    subindo ex ir

--- imposto_renda.php
+++ imposto_renda.php
@@ -6,29 +6,28 @@
 	$vd = $vb - $v;
 	$vir = 0;
 	$vl = 0;
-	function daliqota($num, $d) {
-		$vir = $num*$d / 100;
-		$vl = $num - $vir;
-	}
+	$res = 0;
 
-	if(	$vd < 24280.80) {
-		$res = 0;
+	if(	$vd < 2428.80) {
+		echo('0');;
 	} elseif ($vd < 2428.81 && $vd > 2826.65) {
-		$res = daliqota($vd, 7.5);
+		$res = 7.5;
 	} elseif ($vd < 2826.66 && $vd > 3751.05) {
-		$res = daliqota($vd, 15);
+		$res = 15;
 	} elseif ($vd < 3751.06 && $vd > 4664.68) {
-		$res = daliqota($vd, 22.5);
+		$res = 22.5;
 	}	elseif ($vd > 4664.68) {
-		$res = daliqota($vd, 27.5);
+		$res = 27.5;
 	}
- 	function resultado($vb, $v, $resp, $vl) {
+ 	function resultado($vb, $v, $vir, $vd,$res, $vl) {
+ 		$vir = $vd*$res/100;
+ 		$vl = $vd - $vir;
  		echo("<br/> Sálario Folha: R$$vb");
- 		echo("<br/> Desconto INSS: R$v");
- 		echo("<br/> Desconto IR: R$$resp");
+ 		echo("<br/> Desconto INSS: R$$v");
+ 		echo("<br/> Desconto IR: R$$vir");
  		echo("<br/> Salário Líquido: R$$vl");
  	}
 
- 	echo(resultado($vb, $v, $vd,$res, $vl));
+ 	echo(resultado($vb, $v, $vir,$vd,$res, $vl));
  ?>
 
