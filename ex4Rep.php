62682a7f3b1442d9e0332f54f12c7e218dd82028
richardfreiree <richardzzfreirezz@gmail.com>
2025-09-09 21:05:46 -0300

    subindo ex php

---
+++ ex4Rep.php
@@ -0,0 +1,25 @@
+<?php 
+
+$num1 = 6;
+$num2 = 5;
+$num3 = 3;
+$fi = 0;
+$ft = false;
+while ($ft != true) {
+	if($num1 > $num2){
+		$fi = $num2;
+		$num2 = $num1;
+		$num1 = $fi;
+
+	}elseif ($num2 > $num3) {
+		$fi = $num3;
+		$num3 = $num2;
+		$num2 = $fi;
+	} else{
+		$ft = true;
+		echo "$num1 $num2 $num3";
+	}
+
+};
+
+ ?>
